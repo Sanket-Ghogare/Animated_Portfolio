e8c645d23582155f04da96111047be1dd831afd1
rohanghogare <rohanghogare2710@gmail.com>
2024-02-09 18:36:59 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,3 @@
-// import { useState } from 'react'
-// import "./app.scss"
-// import Heros from './componenets/Navbar/hero/Heros';
-// import Navbar from './componenets/Navbar/Navbar';
-// import Contact from './componenets/Navbar/Contact';
-// import Test from './componenets/Navbar/Test';
-// import Sidebar from './componenets/Navbar/sidebar/Sidebar';
-// import Services from './componenets/Services';
-// import Parallax from './componenets/Parallax';
-// const App = () =>{
-// return (
-// <>
-// <div>
-//     <section id='homePage'>
-//       <Navbar/>
-//       <Heros/>
-//     </section>
-    
-// <Sidebar/>
-//     {/* <section id= 'Services'>Parallax</section> */}
-//     <section id ='Services'>
-// <Services/>
-//     </section>
-//      <section id='Portfolio' ><Parallax type= 'Portfolio'/></section>
-
-   
-// {/*     
-//      <section id ='Test'>
-// <Test/>
-//     </section>  */}
-
-// <section id ='Contact'>
-// <Contact/>
-//     </section>
-
-    
-//     <section id = 'About'>
-//        About
-//     </section>
-//   </div>
-//     </>
-// )
-  
-// };
-
-// export default App
 
 import { useState } from 'react'
 import "./app.scss"
@@ -60,29 +14,40 @@ const App = () =>{
 return (
 <>
 <div>
-    <section id='homePage '>
+    <section id='homePage'>
       <Navbar/> 
        <Heros/>
     </section>
     
 <Sidebar/> 
-<section id='Services'>
-    <Parallax type="services"/>
+<section>
+      <Parallax type ="services"/>
     </section>
-    <section id= 'Services' style={{marginBottom:"30%" , marginTop:"0%"}}> <Services/></section>
-
-    <section id='Portfilio'>
-    <Parallax type="portfilio"/>
+<section id='Services'>
+    <Services type ="services"/>
+  
+    </section>
+   
+    {/* <section id= 'Services' style={{marginBottom:"30%" , marginTop:"0%"}}> <Services/></section> */}
+    <section>
+  <Parallax type="portfolio"/>
+</section>
+    <section id='Portfolio'>
+    <Portfolio type= 'portfolio'/>
    </section>
+<section id='Portfolio'>
+  <Portfolio type="portfolio"/>
+</section>
+   {/* <section className='About'>
+   <Portfolio/>
+   </section> */}
 
-   <section className='abc'></section>
+    
 
-    <Portfolio/>
-
-    <section id='Contact'>
+    {/* <section id='Contact'>
     <Contact/>
 
-    </section>
+    </section> */}
     {/* <section id='Portfolio' ></section>
     <section className='section'>
        <Test/>
@@ -93,10 +58,91 @@ return (
     </section> */}
     
     {/* <Test/> */}
+    {/* <Portfolio/> */}
   </div>
     </>
 )
   
 };
 
-export default App
+export default  App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import "./app.scss"
+// import Heros from './componenets/Navbar/hero/Heros';
+// import Navbar from './componenets/Navbar/Navbar';
+// import Contact from './componenets/Navbar/Contact';
+// import Test from './componenets/Navbar/Test';
+// import Sidebar from './componenets/Navbar/sidebar/Sidebar';
+// import Services from './componenets/Services';
+// import Parallax from './componenets/Parallax';
+// import Portfolio from './componenets/portfolio/Portfolio';
+
+// const App = () =>{
+// return (
+// <>
+// <div>
+//     <section id='homePage'>
+//       <Navbar/> 
+//        <Heros/>
+//     </section>
+    
+// <Sidebar/> 
+// <section>
+//       <Parallax type ="services"/>
+//     </section>
+// <section id='Services'>
+//     <Services type ="services"/>
+  
+//     </section>
+   
+//     {/* <section id= 'Services' style={{marginBottom:"30%" , marginTop:"0%"}}> <Services/></section> */}
+//     <section>
+//   <Parallax type="portfolio"/>
+// </section>
+//     <section id='Portfolio'>
+//     <Portfolio type= 'portfolio'/>
+//    </section>
+// <section id='Portfolio'>
+//   <Portfolio type="portfolio"/>
+// </section>
+//    {/* <section className='About'>
+//    <Portfolio/>
+//    </section> */}
+
+    
+
+//     <section id='Contact'>
+//     <Contact/>
+
+//     </section>
+//     {/* <section id='Portfolio' ></section>
+//     <section className='section'>
+//        <Test/>
+//     </section>
+//     <section id='Contact'>
+//     <Contact/>
+
+//     </section> */}
+    
+//     {/* <Test/> */}
+//   </div>
+//     </>
+// )
+  
+// };
+
+// export default  App;
